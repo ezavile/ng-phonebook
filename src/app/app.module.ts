723ed096ec20d96fa259e8ce4a9b211776d31750
ezavile <ezavile@gmail.com>
2017-09-06 19:53:12 -0500

    added HomeComponent and PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
