2e2c5631f2af12228a0caed498841df2b53e06f8
ezavile <ezavile@gmail.com>
2017-09-07 21:29:38 -0500

    added custom validation

--- src/app/contacts/contact-add/contact-add.component.ts
+++ src/app/contacts/contact-add/contact-add.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
+function dontMensoWordValidation(c: AbstractControl): {[key: string]: boolean} | null {
+  if (c.value.toLowerCase().includes('menso')) {
+    return { 'dontMensoWord': true };
+  }
+  return null;
+}
+
+function dontMentionItValidation(word: string): ValidatorFn {
+  return (c: AbstractControl): {[key: string]: boolean} | null => {
+    if (c.value.toLowerCase().includes(word)) {
+      return { 'dontMentionIt': true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'pb-contact-add',
@@ -13,8 +29,8 @@ export class ContactAddComponent implements OnInit {
 
   ngOnInit() {
     this.contactForm = this.fb.group({
-      firstname: ['', [Validators.required, Validators.minLength(3)]],
-      lastname: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(3), dontMensoWordValidation]],
+      lastname: ['', [Validators.required, dontMentionItValidation('wey')]],
       address: this.fb.group({
         street: '',
         city: '',
