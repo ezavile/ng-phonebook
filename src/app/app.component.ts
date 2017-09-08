1a67332b418f54617bd4fcb74e1c140ecc5582ef
ezavile <ezavile@gmail.com>
2017-09-08 02:52:41 -0500

    added spinner and routerLinkActive directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'pb-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageTitle: string = 'NgPhonebook';
+  loading: boolean = true;
+
+  constructor(private router: Router) {
+    this.router
+      .events
+      .subscribe(
+        (routerEvent: Event) => {
+          this.checkRouterEvent(routerEvent);
+        }
+      );
+  }
+
+  private checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { SpinnerModule } from 'angular2-spinner/dist';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     ContactModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    SpinnerModule
   ],
   declarations: [
     AppComponent,
